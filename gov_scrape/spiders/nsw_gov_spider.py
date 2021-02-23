71c9c8c020ee72d9411f1aefc242a73876083602
Callum Skeet <callum@elitemail.org>
2021-02-23 14:05:19 +1100

    add nsw news spider

---
+++ gov_scrape/spiders/nsw_gov_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+from ..items import RssFeedItem
+from .nsw_prem_spider import NswPremSpider
+
+
+class NswGovSpider(NswPremSpider):
+    name = "nsw_gov_spider"
+
+    custom_settings = {
+        "FEED_FILE": "./feeds/nsw-gov.rss",
+        "FEED_TITLE": "News | NSW Government",
+        "FEED_LINK": "https://www.nsw.gov.au/news",
+        "FEED_DESCRIPTION": "Posts from the New South Wales State Government's news centre",
+        "FEED_EXPORTER": "gov_scrape.exporters.NswGovRssItemExporter",
+    }
+
+    def start_requests(self):
+        url = "https://www.nsw.gov.au/news"
+        yield scrapy.Request(url, self.parse)
