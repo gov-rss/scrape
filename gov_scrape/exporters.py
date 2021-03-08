82769625703a498dfdb6dc77d593dc36c94df9d6
Callum Skeet <callum@elitemail.org>
2021-03-08 17:46:50 +1100

    organise classes alphabetically

--- gov_scrape/exporters.py
+++ gov_scrape/exporters.py
@@ -1,27 +1,27 @@
 from scrapy_rss.exporters import RssItemExporter
 
 
-class VicGovRssItemExporter(RssItemExporter):
-    def __init__(self, *args, **kwargs):
-        kwargs["language"] = kwargs.get("language", "en-AU")
-        kwargs["copyright"] = kwargs.get(
-            "copyright", "Copyright State Government of Victoria"
-        )
-        super().__init__(*args, **kwargs)
-
-
-class VicShadowRssItemExporter(RssItemExporter):
-    def __init__(self, *args, **kwargs):
-        kwargs["language"] = kwargs.get("language", "en-AU")
-        kwargs["copyright"] = kwargs.get("copyright", "Copyright Liberal Victoria")
-        super().__init__(*args, **kwargs)
-
-
 class NswGovRssItemExporter(RssItemExporter):
     def __init__(self, *args, **kwargs):
         kwargs["language"] = kwargs.get("language", "en-AU")
         kwargs["copyright"] = kwargs.get(
             "copyright", "Copyright State Government of New South Wales"
+        )
+        super().__init__(*args, **kwargs)
+
+
+class NtShadowRssItemExporter(RssItemExporter):
+    def __init__(self, *args, **kwargs):
+        kwargs["language"] = kwargs.get("language", "en-AU")
+        kwargs["copyright"] = kwargs.get("copyright", "Copyright Country Liberal Party")
+        super().__init__(*args, **kwargs)
+
+
+class SaGovRssItemExporter(RssItemExporter):
+    def __init__(self, *args, **kwargs):
+        kwargs["language"] = kwargs.get("language", "en-AU")
+        kwargs["copyright"] = kwargs.get(
+            "copyright", "Copyright State Government of South Australia"
         )
         super().__init__(*args, **kwargs)
 
@@ -40,38 +40,6 @@ class QldShadowRssItemExporter(RssItemExporter):
         kwargs["language"] = kwargs.get("language", "en-AU")
         kwargs["copyright"] = kwargs.get(
             "copyright", "Copyright Liberal National Party"
-        )
-        super().__init__(*args, **kwargs)
-
-
-class NtShadowRssItemExporter(RssItemExporter):
-    def __init__(self, *args, **kwargs):
-        kwargs["language"] = kwargs.get("language", "en-AU")
-        kwargs["copyright"] = kwargs.get("copyright", "Copyright Country Liberal Party")
-        super().__init__(*args, **kwargs)
-
-
-class WaGovRssItemExporter(RssItemExporter):
-    def __init__(self, *args, **kwargs):
-        kwargs["language"] = kwargs.get("language", "en-AU")
-        kwargs["copyright"] = kwargs.get(
-            "copyright", "Copyright State Government of Western Australia"
-        )
-        super().__init__(*args, **kwargs)
-
-
-class WaShadowRssItemExporter(RssItemExporter):
-    def __init__(self, *args, **kwargs):
-        kwargs["language"] = kwargs.get("language", "en-AU")
-        kwargs["copyright"] = kwargs.get("copyright", "Liberal Party Western Australia")
-        super().__init__(*args, **kwargs)
-
-
-class SaGovRssItemExporter(RssItemExporter):
-    def __init__(self, *args, **kwargs):
-        kwargs["language"] = kwargs.get("language", "en-AU")
-        kwargs["copyright"] = kwargs.get(
-            "copyright", "Copyright State Government of South Australia"
         )
         super().__init__(*args, **kwargs)
 
@@ -96,4 +64,36 @@ class TasShadowRssItemExporter(RssItemExporter):
     def __init__(self, *args, **kwargs):
         kwargs["language"] = kwargs.get("language", "en-AU")
         kwargs["copyright"] = kwargs.get("copyright", "Copyright Tasmanian Labor")
+        super().__init__(*args, **kwargs)
+
+
+class VicGovRssItemExporter(RssItemExporter):
+    def __init__(self, *args, **kwargs):
+        kwargs["language"] = kwargs.get("language", "en-AU")
+        kwargs["copyright"] = kwargs.get(
+            "copyright", "Copyright State Government of Victoria"
+        )
+        super().__init__(*args, **kwargs)
+
+
+class VicShadowRssItemExporter(RssItemExporter):
+    def __init__(self, *args, **kwargs):
+        kwargs["language"] = kwargs.get("language", "en-AU")
+        kwargs["copyright"] = kwargs.get("copyright", "Copyright Liberal Victoria")
+        super().__init__(*args, **kwargs)
+
+
+class WaGovRssItemExporter(RssItemExporter):
+    def __init__(self, *args, **kwargs):
+        kwargs["language"] = kwargs.get("language", "en-AU")
+        kwargs["copyright"] = kwargs.get(
+            "copyright", "Copyright State Government of Western Australia"
+        )
+        super().__init__(*args, **kwargs)
+
+
+class WaShadowRssItemExporter(RssItemExporter):
+    def __init__(self, *args, **kwargs):
+        kwargs["language"] = kwargs.get("language", "en-AU")
+        kwargs["copyright"] = kwargs.get("copyright", "Liberal Party Western Australia")
         super().__init__(*args, **kwargs)
